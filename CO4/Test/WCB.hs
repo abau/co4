6fb1f5424e20140a9c9a27cfcf20b6b2f185312f
apunktbau <a.bau@gmx.net>
2013-06-06 15:04:15 +0200

    WCB

---
+++ CO4/Test/WCB.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module CO4.Example.WCB
+where
+
+import           Language.Haskell.TH (runIO)
+import qualified Satchmo.Core.SAT.Minisat
+import qualified Satchmo.Core.Decode 
+import           CO4
+import           CO4.Prelude
+import           CO4.Util (toBinary,fromBinary)
+
+$( runIO $ configurable [ImportPrelude] $ compileFile "CO4/Test/WCB.standalone.hs" )
+
+
+uBase = constructors [ Just [], Just [], Just [], Just []]
+
+kList 0 a = known 0 2 []
+kList i a = known 1 2 [ a , kList (i-1) a]
+
+allocator = kList 4 uBase
+
+result = solveAndTestBooleanP (Finite (toBinary Nothing 4)) allocator encMain main 
---
+++ CO4/Test/WCB.standalone.hs
@@ -0,0 +1,166 @@
+module WCB where
+
+data Base = A | C | G | U
+
+type Primary = [Base]
+
+data Paren = Open | Close | Blank
+
+type Secondary = [Paren]
+
+type Bit = Bool
+type Nat = [Bit]
+
+data Maybe' a = Nothing' | Just' a
+
+data Energy = MinusInfinity | Finite Nat --deriving Show
+
+main e p = geEnergy (maxbound p) e
+
+geEnergy a b = case b of
+  MinusInfinity -> True
+  Finite b' -> case a of 
+    MinusInfinity -> False
+    Finite a' -> ge a' b'
+
+plus :: Energy -> Energy -> Energy
+plus e f = case e of
+  Finite x -> case f of 
+    Finite y      -> Finite (maxNat x y)
+    MinusInfinity -> e
+  MinusInfinity -> f
+
+times :: Energy -> Energy -> Energy
+times e f = case e of
+  Finite x -> case f of 
+    Finite y      -> Finite (add' x y)
+    MinusInfinity -> MinusInfinity
+  MinusInfinity -> MinusInfinity
+
+maxbound :: Primary -> Energy
+maxbound p = case p of
+  []     -> Finite []
+  (x:xs) -> case xs of
+    [] -> Finite []
+    _  -> foldr plus (group p) (splittings p)
+
+
+group :: Primary -> Energy
+group p = case p of
+  []     -> MinusInfinity
+  (x:xs) -> case last' p of
+    Nothing' -> MinusInfinity
+    Just' l  -> case init' xs of
+                  Nothing' -> MinusInfinity
+                  Just' is -> times (cost x l) (maxbound is)
+
+init' :: [a] -> Maybe [a]
+init' xs = case xs of
+  []   -> Nothing'
+  y:ys -> case ys of
+    [] -> Just' [y]
+    _  -> case init' ys of 
+            Nothing' -> Nothing'
+            Just' ys' -> Just' (y : ys')
+                
+last' :: [a] -> Maybe' a
+last' a = case a of
+  []     -> Nothing'
+  (x:xs) -> case xs of
+    [] -> Just' x
+    _  -> last' xs
+
+cost :: Base -> Base -> Energy
+cost b1 b2 = case b1 of
+  A -> case b2 of U -> Finite [True]
+                  _ -> MinusInfinity
+  U -> case b2 of A -> Finite [True]
+                  G -> Finite [True]
+                  _ -> MinusInfinity
+  G -> case b2 of U -> Finite [True]
+                  C -> Finite [False,True]
+                  _ -> MinusInfinity
+  C -> case b2 of G -> Finite [False,True]
+                  _ -> MinusInfinity
+
+splittings :: Primary -> [Energy]
+splittings p = map (\(p1,p2) -> times (maxbound p1) (maxbound p2)) 
+                   (filter' (\(p1,p2) -> not (null' p1) && not (null' p2)) 
+                   (zip (inits' p) (tails' p)))
+
+null' :: [a] -> Bool
+null' xs = case xs of
+  [] -> True
+  _  -> False
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' f xs = case xs of
+  []   -> []
+  y:ys -> case f y of
+            False -> filter' f ys
+            True -> y : (filter' f ys)
+
+inits' :: [a] -> [[a]]
+inits' xs = case xs of
+  []     -> [[]]
+  (x:xs) -> [] : (map (\ys -> x:ys) (inits' xs ))
+  
+tails' :: [a] -> [[a]]
+tails' xs = case xs of
+  []     -> [[]]
+  (y:ys) -> xs : tails' ys
+  
+maxNat :: Nat -> Nat -> Nat
+maxNat xs ys = case ge xs ys of
+              False -> ys
+              True  -> xs
+
+minNat :: Nat -> Nat -> Nat
+minNat xs ys = case ge xs ys of
+              False -> xs
+              True  -> ys
+
+ge :: Nat -> Nat -> Bool
+ge xs ys = foldl (\f (x,y) -> (x && (not y)) || ((not (xor x y)) && f)) True 
+                  (case fill xs ys of
+                      (a',b') -> zip a' b'
+                  )
+
+add' :: Nat -> Nat -> Nat
+add' a b = case add a b of
+  (r,c) -> r ++ [c]
+
+add :: Nat -> Nat -> (Nat,Bit)
+add a b = foldl (\(result,carry) (a,b) -> 
+                    case fullAdder a b carry of
+                      (r, carry') -> (result ++ [r], carry')
+                ) ([],False) 
+                  (case fill a b of
+                      (a',b') -> zip a' b'
+                  )
+
+fill :: Nat -> Nat -> (Nat,Nat)
+fill xs ys = case xs of
+  []     -> case ys of []   -> ([],[])
+                       v:vs -> case fill [] vs of
+                                 (us',vs') -> (False : us', v : vs')
+  u:us -> case ys of [] -> case fill us [] of
+                             (us',vs') -> (u : us', False : vs')
+                     v:vs -> case fill us vs of
+                               (us',vs') -> (u:us', v:vs')
+
+fullAdder :: Bit -> Bit -> Bit -> (Bit,Bit)
+fullAdder a b carry =
+  let xorAB = xor a b
+  in
+    ( xor xorAB carry
+    , xor (a && b) (carry && xorAB)
+    )
+
+halfAdder :: Bit -> Bit -> (Bit,Bit)
+halfAdder a b = (xor a b, a && b)
+  
+xor :: Bit -> Bit -> Bit
+xor a b = case a of
+  False -> b
+  True  -> not b
