8c91b57185c6a7d54c284c0c91667de4fe863b2e
apunktbau <a.bau@gmx.net>
2013-06-06 07:03:37 +0200

    add example Binary.hs

---
+++ CO4/Example/Binary.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module CO4.Example.Binary
+where
+
+import           Language.Haskell.TH (runIO)
+import qualified Satchmo.Core.SAT.Minisat
+import qualified Satchmo.Core.Decode 
+import           CO4
+import           CO4.Prelude
+import           CO4.Util (toBinary,fromBinary)
+
+$( runIO $ configurable [ImportPrelude] $ compileFile "CO4/Example/Binary.standalone.hs" )
+
+bitWidth  = 8
+uNat      = uList bitWidth uBool
+allocator = uTuple2 uNat uNat
+
+result x = do
+  solution <- solveAndTestBooleanP (toBinary (Just bitWidth) x) allocator encMain main 
+  case solution of
+    Nothing    -> return Nothing
+    Just (a,b) -> return $ Just (fromBinary a, fromBinary b)
---
+++ CO4/Example/Binary.standalone.hs
@@ -0,0 +1,73 @@
+module Binary where
+
+type Bit = Bool
+type Nat = [Bit]
+
+main r (a,b) = case mult a b of
+  (result,carry) -> and [ result == r
+                        , not (trivial a)
+                        , not (trivial b)
+                        , not carry
+                        ]
+trivial a = case a of
+  []   -> True
+  (:) x xs -> all (\bit -> bit == False) xs
+
+mult :: Nat -> Nat -> (Nat,Bit)
+mult a b = case a of
+  []     -> ([],False)
+  (x:xs) -> 
+    let multResult  = mult xs b
+        shiftResult = withCarry shiftRight multResult
+    in
+      case x of
+        False -> shiftResult
+        True  -> withCarry (add b) shiftResult
+
+withCarry :: (Nat -> (Nat,Bit)) -> (Nat,Bit) -> (Nat,Bit)
+withCarry f (n,carry) = case f n of
+  (result, carry') -> (result, carry || carry')
+
+shiftRight :: Nat -> (Nat,Bit)
+shiftRight xs = cutMsb (False : xs)
+  where
+    cutMsb xs = case xs of
+      []   -> ([],False) -- never happens
+      y:ys -> case ys of
+                [] -> ([],y)
+                _  -> case cutMsb ys of 
+                        (result,carry) -> (y : result,carry) 
+
+add :: Nat -> Nat -> (Nat,Bit)
+add a b = foldl (\(result,carry) (a,b) -> 
+                    case fullAdder a b carry of
+                      (r, carry') -> (result ++ [r], carry')
+                ) ([],False) 
+                  (case fill a b of
+                      (a',b') -> zip a' b'
+                  )
+
+fill :: Nat -> Nat -> (Nat,Nat)
+fill xs ys = case xs of
+  []     -> case ys of []   -> ([],[])
+                       v:vs -> case fill [] vs of
+                                 (us',vs') -> (False : us', v : vs')
+  u:us -> case ys of [] -> case fill us [] of
+                             (us',vs') -> (u : us', False : vs')
+                     v:vs -> case fill us vs of
+                               (us',vs') -> (u:us', v:vs')
+fullAdder :: Bit -> Bit -> Bit -> (Bit,Bit)
+fullAdder a b carry =
+  let xorAB = xor a b
+  in
+    ( xor xorAB carry
+    , xor (a && b) (carry && xorAB)
+    )
+
+halfAdder :: Bit -> Bit -> (Bit,Bit)
+halfAdder a b = (xor a b, a && b)
+  
+xor :: Bit -> Bit -> Bit
+xor a b = case a of
+  False -> b
+  True  -> not b
