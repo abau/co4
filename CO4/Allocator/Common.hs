4cd848d04284a799f628f2d3f70d38645f2af99c
apunktbau <a.bau@gmx.net>
2013-06-13 16:22:18 +0200

    add pretty tree-shaped Show instances for allocators

--- CO4/Allocator/Common.hs
+++ CO4/Allocator/Common.hs
@@ -5,17 +5,38 @@ module CO4.Allocator.Common
 where
 
 import qualified Control.Exception as Exception
+import           Data.Tree
 
 data Allocator = Known { _constructorIndex :: Int
                        , _numConstructors  :: Int
                        , _arguments        :: [Allocator]
                        }
                | Unknown [AllocateConstructor]
-               deriving (Eq,Ord,Show)
+               deriving (Eq,Ord)
 
 data AllocateConstructor = AllocateConstructor [Allocator]
                          | AllocateBottom
-               deriving (Eq,Ord,Show)
+               deriving (Eq,Ord)
+
+instance Show Allocator where
+  show = drawTree . toTree
+
+toTree :: Allocator -> Tree String
+toTree allocator = case allocator of
+  Known i n args -> Node (unwords ["Known",show i,show n]) 
+                  $ zipWith argToTree [0..] args
+  Unknown cons   -> Node "Unknown" $ zipWith consToTree [0..] cons
+  where
+    argToTree i (Known j n args) = 
+      Node (show i ++ unwords ["th argument: known",show j,show n]) 
+          $ zipWith argToTree [0..] args
+    argToTree i (Unknown cons) = 
+      Node (show i ++ "th argument: unknown") $ zipWith consToTree [0..] cons
+
+    consToTree i (AllocateConstructor args) = 
+      Node (show i ++ "th constructor") $ zipWith argToTree [0..] args
+    consToTree i AllocateBottom = 
+      Node (show i ++ "th constructor: bottom") []
 
 known :: Int -> Int -> [Allocator] -> Allocator
 known = Known
