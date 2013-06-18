93855ff06397d8c9ef5e3b04378d970b3c02bb13
apunktbau <a.bau@gmx.net>
2013-06-18 18:29:25 +0200

    fix profiling

--- CO4/EncEq.hs
+++ CO4/EncEq.hs
@@ -1,12 +1,17 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 module CO4.EncEq 
-  (EncEq (..))
+  (EncEq (..), EncProfiledEq (..))
 where
 
 import Satchmo.Core.MonadSAT (MonadSAT)
 import Satchmo.Core.Primitive (Primitive)
 import CO4.EncodedAdt (EncodedAdt)
 import CO4.Cache (MonadCache)
+import CO4.Profiling (MonadProfiling)
 
 class (Primitive p, EncodedAdt e p) => EncEq a e p where
   encEq :: (MonadSAT m, MonadCache (e p) m) => a -> e p -> e p -> m (e p)
+
+class (Primitive p, EncodedAdt e p) => EncProfiledEq a e p where
+  encProfiledEq :: (MonadSAT m, MonadCache (e p) m, MonadProfiling m) 
+                => a -> e p -> e p -> m (e p)
