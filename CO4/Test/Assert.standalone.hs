7ad611704673063f256e2dbe17886bf0aa20f647
Johannes Waldmann <waldmann@imn.htwk-leipzig.de>
2013-06-24 15:07:37 +0200

    test case for profiling

--- CO4/Test/Assert.standalone.hs
+++ CO4/Test/Assert.standalone.hs
@@ -3,8 +3,11 @@ module CO4.Test.WCB_Matrix where
 import CO4.Prelude
 import CO4.Test.WCB_Nat8
 
-constraint s xs = eqNat8 s ( summe xs)
+constraint s xs = 
+    eqNat8 s ( summe xs )
 
-summe xs = case assertKnown xs of
+summe xs = case  xs of
     [] -> nat8 0
-    x:xs' -> plusNat8 x (summe xs')
+    x:xs' -> plus x (summe xs')
+
+plus = plusNat8
