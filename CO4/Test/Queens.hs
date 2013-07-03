cb4ece1cb9e946e4a87765d67acee89e1ef18b2c
apunktbau <a.bau@gmx.net>
2013-07-03 17:07:04 +0200

    fix queens example

--- CO4/Test/Queens.hs
+++ CO4/Test/Queens.hs
@@ -18,36 +18,25 @@ import System.Environment (getArgs)
 import System.IO
 
 $( runIO $ configurable [ ImportPrelude
-                        , Profile
+                        --, Profile
+                        , Cache
                         -- ,DumpAll "/tmp/WCB"
                         ] 
          $ compileFile "CO4/Test/Queens.standalone.hs")
 
-kList 0 a = known 0 2 []
-kList i a = known 1 2 [ a , kList (i-1) a]
-
-uNat w = kList w uBool
-
-toBin :: Int -> [Bool]
-toBin x = 
-    if x == 0 then [] 
-    else let (d,m) = divMod x 2 in odd m : toBin d
-
-fromBin :: [Bool] -> Int
-fromBin xs = foldr ( \ x y -> fromEnum x + 2*y ) 0 xs
+uNat w = uNat8
 
 result n = do
     hSetBuffering stdout LineBuffering
-    let xs = toBin n ; w = length xs
     solution <- solveAndTestP 
-       xs
-       (kList n (uNat w))
-       encMain main
+       (nat8 n)
+       (kList n uNat8)
+       encConstraint constraint
     case solution of
         Nothing -> putStrLn "no placement"
-        Just xs -> print (map fromBin xs)
+        Just xs -> print xs
 
-mainz = do
+main = do
     [s] <- getArgs
     result $ read s
 
--- CO4/Test/Queens.standalone.hs
+++ CO4/Test/Queens.standalone.hs
@@ -1,68 +1,27 @@
 
-type Bit = Bool
-type Nat = [Bit]
-
-main n xs = 
-     all ( \ x -> le one x && le x n ) xs
+constraint :: Nat8 -> [Nat8] -> Bool
+constraint n xs = 
+     all ( \ x -> le (nat8 1) x && le x n ) xs
   && all_safe xs
 
+all_safe :: [Nat8] -> Bool
 all_safe xs = case xs of
     [] -> True
-    x:xs' -> safe x xs' one && all_safe xs'
+    x:xs' -> safe x xs' (nat8 1) && all_safe xs'
 
+safe :: Nat8 -> [Nat8] -> Nat8 -> Bool
 safe x ys p = case ys of
     [] -> True
     y : ys' -> no_attack x y p 
         && safe x ys' (increment p)
 
+no_attack :: Nat8 -> Nat8 -> Nat8 -> Bool
 no_attack x y p = 
     neq x y && neq (add x p) y && neq x (add y p)
 
--- boilerplat for handling numbers follows ------------------
+le = leNat8
+neq a b = not (eqNat8 a b)
+add = plusNat8
 
-one = [True]
-
-eqNat xs ys = le xs ys && le ys xs
-
-neq xs ys = not (eqNat xs ys)
-
-gt xs ys = not (le xs ys)
-
-le xs ys = ge ys xs
-
-ge :: Nat -> Nat -> Bool
-ge xs ys = ge_run True xs ys
-
-ge_run prev xs ys = case xs of
-    [] -> prev && not ( or ys )
-    x : xs' ->  case ys of
-        [] -> prev || or xs
-        y : ys' -> ge_run ((x && not y) || (prev && (x == y))) xs' ys'
-
-add :: Nat -> Nat -> Nat
-add xs ys = add_with False xs ys
-
-add_with c xs ys = case xs of
-    [] -> increment_with c ys
-    x : xs' -> case ys of
-        [] -> increment_with c xs
-        y : ys' -> ( xor3 c x y ) : add_with (atleast2 c x y) xs' ys'
-
-increment xs = increment_with True xs
-
-increment_with c xs = case xs of
-    [] -> [c]
-    x : xs' -> (xor2 c x) : increment_with ( c && x) xs'
-
-atleast2 x y z = or3 (x && y) (x && z) (y && z)
-
-or3 :: Bit -> Bit -> Bit -> Bit
-or3 x y z = x || (y || z)
-
-xor3 :: Bit -> Bit -> Bit -> Bit
-xor3 x y z = xor2 x (xor2 y z)
-  
-xor2 :: Bit -> Bit -> Bit
-xor2 a b = case a of
-  False -> b
-  True  -> not b
+increment :: Nat8 -> Nat8
+increment x = add x (nat8 1)
