406cb808b3caacc9515dd3789f841ee7b9a2693b
Johannes Waldmann <waldmann@imn.htwk-leipzig.de>
2013-09-17 18:49:49 +0200

    wrong

--- CO4/CSPlib/Prob006/Constraint.hs
+++ CO4/CSPlib/Prob006/Constraint.hs
@@ -11,13 +11,66 @@ constraint len diffs =
 
 
 -- naive implementation
-alldifferent xs = case xs of
+alldifferent_naive xs = case xs of
     [] -> True
     x : ys -> all ( \ y -> not (eqNat x y)) ys
           && alldifferent ys
 
+alldifferent xs = 
+   neighbour_different ( oesort xs )
+
+neighbour_different xs = case xs of
+    [] -> True
+    x : xs' -> case xs' of
+        [] -> True
+        y : ys' -> 
+            not (eqNat x y) && neighbour_different xs'
+
+-- FIXME
+oesort xs = case xs of
+    [] -> []
+    x : xs' -> case xs' of
+        [] -> [x]
+        x' : xs'' -> 
+            let (ys,zs) = distribute xs
+            in  oemerge (oesort ys) (oesort zs)
+
+-- | distribute [0..4] = ([0,2,4],[1,3])
+distribute xs = case xs of
+    [] -> ( [], [] )
+    x : xs' -> let (ys,zs) = distribute xs'
+               in  (x : zs, ys)
+
+
+oemerge xs ys = case xs of
+    [] -> fuse xs ys
+    x : xs' -> case xs' of
+      [] -> fuse xs ys
+      x' : xs'' -> 
+        case ys of
+            [] -> fuse xs ys
+            y : ys' -> case ys' of
+                [] -> fuse xs ys
+                y' : ys'' -> 
+                  let (xes, xos) = distribute xs
+                      (yes, yos) = distribute ys
+                  in  repair (oemerge xes yes)
+                           (oemerge xos yos) 
+
+fuse = repair
+
+repair xs ys = case xs of
+    [] -> ys 
+    x : xs' -> case ys of
+        [] -> xs
+        y : ys' -> case ( case ltNat x y of
+                      True -> (x,y) ; False -> (y,x)
+                 ) of 
+                ( lo, hi ) -> lo : hi : repair xs' ys'
+
+    
 -- equivalent to map sum $ segments xs
--- but used fewer additions (?)
+-- but uses fewer additions (?)
 map_sum_segments xs = case xs of
     [] -> []
     x : xs' -> map_sum_inits xs ++ map_sum_segments xs'
