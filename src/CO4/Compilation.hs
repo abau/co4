e45adbcf41690bf3b35db2c603b92a6ec4f92079
apunktbau <a.bau@gmx.net>
2014-06-04 10:08:14 +0200

    avoid IO monad while compiling

--- src/CO4/Compilation.hs
+++ src/CO4/Compilation.hs
@@ -1,22 +1,26 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 module CO4.Compilation
   (compileFile, compile)
 where
 
 import           Prelude hiding (log)
 import           System.IO (stderr, hPutStrLn)
-import           Control.Monad.Reader 
+import           Control.Monad.Writer
 import qualified Language.Haskell.TH as TH
 import           Language.Haskell.TH.Syntax (Q,addDependentFile)
 import qualified Language.Haskell.Exts as HE
 import           CO4.Language (Program)
-import           CO4.Unique (MonadUnique,withUniqueT)
+import           CO4.Unique (MonadUnique,UniqueT,withUniqueT,liftListen,liftPass)
 import           CO4.THUtil (unqualifiedNames)
 import           CO4.Util (addDeclarations)
 import           CO4.Prelude (parsePrelude)
-import           CO4.Config (MonadConfig,Config(..),configurable,is,fromConfigs)
+import           CO4.Config 
+  (MonadConfig,Config(..),ConfigurableT,configurable,mapConfigurableT,is,fromConfigs)
 import qualified CO4.Config as C
 import           CO4.Algorithms.Globalize (globalize)
 import           CO4.Algorithms.UniqueNames (uniqueLocalNames)
@@ -30,6 +34,8 @@ import           CO4.PPrint (pprint)
 import           CO4.Frontend.TH (parsePreprocessedTHDeclarations)
 import           CO4.Frontend.HaskellSrcExts (toTHDeclarations)
 
+type Message = (Maybe FilePath, String)
+
 compileFile :: [Config] -> FilePath -> Q [TH.Dec]
 compileFile configs filePath = 
   TH.runIO (HE.parseFile filePath) >>= \case
@@ -41,11 +47,19 @@ compileFile configs filePath =
                                 , filePath, "` (", msg, " at ", show loc, ")" ]
 
 compile :: [Config] -> [TH.Dec] -> Q [TH.Dec]
-compile configs program = TH.runIO 
-                        $ configurable configs 
-                        $ withUniqueT 
-                        $ do
+compile configs program = do
+  (program', msg :: [Message]) <- runWriterT $ configurable configs 
+                                             $ withUniqueT 
+                                             $ compile' program
+  forM_ msg $ \case 
+    (Nothing, msg) -> TH.runIO $ hPutStrLn stderr msg
+    (Just fp, msg) -> TH.runIO $ writeFile fp     msg
 
+  return program'
+
+compile' :: (MonadWriter [Message] m, MonadUnique m, MonadConfig m) 
+         => [TH.Dec] -> m [TH.Dec]
+compile' program = do
   parsedPrelude <- is ImportPrelude >>= \case
                         True  -> parsePrelude
                         False -> return []
@@ -69,24 +83,36 @@ compile configs program = TH.runIO
 
         False -> runCodeGenerator codeGen co4Program
 
-      liftIO $ log "Compilation successful"
+      log "Compilation successful"
 
       is HideSource >>= \case
         False -> return $ program ++ result
         True  -> return result
 
-runCodeGenerator :: (MonadUnique m, MonadIO m, MonadConfig m) 
+runCodeGenerator :: (MonadUnique m , MonadWriter [Message] m, MonadConfig m) 
         => (Program -> m [TH.Dec]) -> Program -> m [TH.Dec]
 runCodeGenerator generator program = do
   thProgram <- generator program 
   dump $ show $ TH.ppr $ unqualifiedNames thProgram
   return thProgram
 
-dump :: (MonadIO m, MonadConfig m) => String -> m ()
+dump :: (MonadWriter [Message] m, MonadConfig m) => String -> m ()
 dump msg = fromConfigs C.dumpTo >>= \case
   Nothing -> return ()
   Just "" -> log msg
-  Just f  -> liftIO $ writeFile f msg
+  Just f  -> tell [(Just f, msg)]
 
-log :: MonadIO m => String -> m ()
-log = liftIO . hPutStrLn stderr 
+log :: MonadWriter [Message] m => String -> m ()
+log x = tell [(Nothing, x)]
+
+instance MonadWriter [Message] m => MonadWriter [Message] (UniqueT m) where
+  writer = lift . writer
+  tell   = lift . tell
+  listen = liftListen listen
+  pass   = liftPass pass
+
+instance MonadWriter [Message] m => MonadWriter [Message] (ConfigurableT m) where
+  writer = lift . writer
+  tell   = lift . tell
+  listen = mapConfigurableT listen
+  pass   = mapConfigurableT pass
