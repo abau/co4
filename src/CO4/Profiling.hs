f0db86e1eddd403ecb7633ddf82aeb926e23d430
apunktbau <a.bau@gmx.net>
2014-06-10 10:09:30 +0200

    minor renamings

--- src/CO4/Profiling.hs
+++ src/CO4/Profiling.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE LambdaCase #-}
 module CO4.Profiling 
-  ( ProfileData, numCalls, numVariables, numClauses, incNumVariables, incNumClauses
+  ( numCalls, numVariables, numClauses, incNumVariables, incNumClauses
   , Profile, currentFunction, currentInner, emptyProfile, onCurrentInner
   , resetCurrentInner, callFunction, returnTo, incInnerUnderBy, printProfile
   )
@@ -15,61 +15,61 @@ import qualified Data.Map.Strict as M
 
 import System.IO ( stderr, hPutStrLn)
 
-data ProfileData = ProfileData { numCalls     :: ! Int
-                               , numVariables :: ! Int
-                               , numClauses   :: ! Int
-                               } 
-                               deriving Show
+data FunProfileData = FunProfileData { numCalls     :: ! Int
+                                     , numVariables :: ! Int
+                                     , numClauses   :: ! Int
+                                     } 
+                                     deriving Show
 
-emptyProfileData :: ProfileData
-emptyProfileData = ProfileData 0 0 0
+emptyFunProfileData :: FunProfileData
+emptyFunProfileData = FunProfileData 0 0 0
 
-isEmptyProfileData :: ProfileData -> Bool
-isEmptyProfileData d = and [ numCalls d == 0, numVariables d == 0, numClauses d == 0 ]
+isEmptyFunProfileData :: FunProfileData -> Bool
+isEmptyFunProfileData d = and [ numCalls d == 0, numVariables d == 0, numClauses d == 0 ]
 
-incNumCalls,incNumVariables,incNumClauses :: ProfileData -> ProfileData
+incNumCalls,incNumVariables,incNumClauses :: FunProfileData -> FunProfileData
 incNumCalls     c = c { numCalls     = 1 + numCalls     c }
 incNumVariables c = c { numVariables = 1 + numVariables c }
 incNumClauses   c = c { numClauses   = 1 + numClauses   c }
 
-incAllBy :: Int -> Int -> Int -> ProfileData -> ProfileData
+incAllBy :: Int -> Int -> Int -> FunProfileData -> FunProfileData
 incAllBy calls vars clauses p = 
   p { numCalls     = calls   + numCalls     p
     , numVariables = vars    + numVariables p
     , numClauses   = clauses + numClauses   p
     }
 
-type ProfileMap = M.Map String ProfileData
+type FunProfileMap = M.Map String FunProfileData
 
-data Profile = Profile { innerUnder      :: ! ProfileMap
-                       , inner           :: ! ProfileMap
+data Profile = Profile { innerUnder      :: ! FunProfileMap
+                       , inner           :: ! FunProfileMap
                        , currentFunction :: ! String
-                       , currentInner    :: ! ProfileData
+                       , currentInner    :: ! FunProfileData
                        }
 
 initName :: String
 initName = "__init"
 
 emptyProfile :: Profile
-emptyProfile = Profile M.empty M.empty initName emptyProfileData
+emptyProfile = Profile M.empty M.empty initName emptyFunProfileData
 
-onCurrentInner :: (ProfileData -> ProfileData) -> Profile -> Profile
+onCurrentInner :: (FunProfileData -> FunProfileData) -> Profile -> Profile
 onCurrentInner f p = p { currentInner = f $ currentInner p }
 
 resetCurrentInner :: Profile -> Profile
-resetCurrentInner p = p { currentInner = emptyProfileData }
+resetCurrentInner p = p { currentInner = emptyFunProfileData }
 
 callFunction :: String -> Profile -> Profile
-callFunction name p = assert (isEmptyProfileData $ currentInner p) $
+callFunction name p = assert (isEmptyFunProfileData $ currentInner p) $
   p { currentFunction = name 
     , innerUnder      = M.alter (\case 
-                          Nothing -> Just $ emptyProfileData { numCalls = 1 }
+                          Nothing -> Just $ emptyFunProfileData { numCalls = 1 }
                           Just d  -> Just $ incNumCalls d
                         ) name $ innerUnder p
-    , currentInner    = emptyProfileData { numCalls = 1 }
+    , currentInner    = emptyFunProfileData { numCalls = 1 }
     }
 
-returnTo :: String -> ProfileData -> Profile -> Profile
+returnTo :: String -> FunProfileData -> Profile -> Profile
 returnTo previousName previousInner = returnTo' . writeCurrentInner
   where
     returnTo' p = 
