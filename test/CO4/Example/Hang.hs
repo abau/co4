71dd1f3370c12b05002ee26dd7639dca592c21b5
Johannes Waldmann <waldmann@imn.htwk-leipzig.de>
2015-01-25 14:16:09 +0100

    use existing allocator notation

--- test/CO4/Example/Hang.hs
+++ test/CO4/Example/Hang.hs
@@ -21,24 +21,6 @@ $( compileFile [ ImportPrelude
                ] 
   "test/CO4/Example/HangStandalone.hs" )
 
--- that's what I want to write:
--- uTurn b = Turn <$> complete <*> uNat b
-
-uTurn :: Int -> TAllocator Turn
-uTurn b = unsafeTAllocator
-        $ constructors [ Just [ toAllocator uBool
-                              , toAllocator $ uNat b]]
-
-uBool :: TAllocator Bool
-uBool = complete
-
-uPair :: TAllocator a -> TAllocator b -> TAllocator (a,b)
-uPair a b = unsafeTAllocator
-    $ constructors [ Just [ toAllocator a, toAllocator b ] ]
-
--- uStack :: Int -> Int -> TAllocator Stack
--- uStack d b = uList d $ uTurn b
-
 main = do
   putStrLn $ unlines
     [ "1:n picture hanging problem"
@@ -50,14 +32,14 @@ main = do
 -- | successful example calls:
 --  result 2 4, result 3 10, result 4 16, result 5 28.
 -- The challenge is to find a solution for @s@
--- that is shorter  @bestknown s@ (see below)
+-- that is shorter than @bestknown s@ (see below)
 -- or prove that it's impossible.
 
 result s n = do
   let b = ceiling $ logBase 2 $ fromIntegral s
   out <- -- solveAndTestP
     solveP (nat b $ s - 1)
-      (uPair ( kList n $ uTurn b )
+      (knownTuple2 ( kList n $ knownTurn complete $ uNat b)
           (allocatorList $ map (\p -> fromKnown $ nat b p) [0..s-1])
        )
       encConstraint
