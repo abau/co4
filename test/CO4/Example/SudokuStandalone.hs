396624c2068719621d73f40153d4553e0bcab4c1
abau <dev@abau.org>
2016-05-13 06:58:01 +0200

    sudoku: refactor allDifferent predicate

--- test/CO4/Example/SudokuStandalone.hs
+++ test/CO4/Example/SudokuStandalone.hs
@@ -11,6 +11,8 @@ type Board    = Matrix Block
 constraint :: Board -> Bool
 constraint board = 
   let n = unaryLength board
+      n2 = unaryLength $ concat board
+      allDifferent xs = allOccur (unaryBelow n2) xs
   in
     and [ all (all (\b -> allDifferent $ concat b)) board
         , all allDifferent $ rows board n
@@ -29,15 +31,22 @@ columns board n =
   in
     rows transposedBoard n
 
-allDifferent :: [Unary] -> Bool
-allDifferent xs = case xs of
-  [] -> True
-  y:ys -> (allDifferent ys) && (not $ contains y ys)
+allOccur :: [Unary] -> [Unary] -> Bool
+allOccur ns xs = all (\n -> exactlyOne (map (unaryEq n) xs)) ns
 
-contains :: Unary -> [Unary] -> Bool
-contains x xs = case xs of
+exactlyOne :: [Bool] -> Bool
+exactlyOne xs = case xs of
   [] -> False
-  y:ys -> (unaryEq x y) || (contains x ys)
+  x:xs' -> case xs' of
+    [] -> x
+    x':xs'' -> case split xs of
+      (ys,zs) -> (exactlyOne ys && none zs) || (exactlyOne zs && none ys)
+
+split :: [a] -> ([a], [a])
+split xs = case xs of
+  [] -> ([],[])
+  x:xs' -> case split xs' of
+    (ys,zs) -> (x:zs,ys)
 
 at :: [a] -> Unary -> a
 at xs i = case i of
@@ -60,6 +69,14 @@ unaryEq a b = case a of
                  _ -> False
   S a' -> case b of Z -> False
                     S b' -> unaryEq a' b'
+
+unaryBelow :: Unary -> [Unary]
+unaryBelow x = case x of
+  Z -> []
+  S x' -> x' : unaryBelow x'
+
+none :: [Bool] -> Bool
+none xs = not $ or xs
 
 transpose :: Matrix a -> Matrix a
 transpose xs = case xs of
